81a14bc3386988f7ae32d77ecb1fb6ce480dfc98
blukat29 <yunjong@kaist.ac.kr>
2014-07-19 23:10:51 +0900

    Fully implement slack OAuth2.0.

--- notifyhere/dash/views.py
+++ notifyhere/dash/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+import secrets
+
 from urllib import quote
+from httplib import HTTPSConnection
 
 BASE_REDIRECT_URL = "http://cert.kaist.in:8000/dash/auth/"
-SLACK_CLIENT_ID = "2160778816.2472356570"
 
 def slack_auth():
     url  = "https://slack.com/oauth/authorize"
-    url += "?client_id=" + SLACK_CLIENT_ID
+    url += "?client_id=" + secrets.SLACK_CLIENT_ID
     url += "&redirect_uri=" + quote(BASE_REDIRECT_URL + "slack")
+    url += "&scope=" + "read"
     return redirect(url)
+
+def slack_access(code):
+    url  = "/api/oauth.access"
+    url += "?client_id=" + secrets.SLACK_CLIENT_ID
+    url += "&client_secret=" + secrets.SLACK_CLIENT_SECRET
+    url += "&code=" + code
+    url += "&redirect_uri=" + quote(BASE_REDIRECT_URL + "slack")
+    conn = HTTPSConnection("slack.com")
+    conn.request("GET", url, "", {})
+    return conn.getresponse().read()
 
 def index(request):
 
@@ -23,7 +37,13 @@ def index(request):
 def auth(request, service=None):
     output  = 'Thx! you authorized my access to your ' + str(service) + ' account'
 
-    if 'code' in request.GET:
+    if 'error' in request.GET:
+        output = 'Ouch! what did I do wrong? you did not authorize me..'
+        return HttpResponse(output)
+
+    elif 'code' in request.GET:
         output += '<br>The code was ' + request.GET['code']
+        tok = slack_access(request.GET['code'])
+        output += '<br>The token is ' + tok
 
     return HttpResponse(output)
