e8c7e96b1c8d123c598fbff171c468600814eb3f
blukat29 <yunjong@kaist.ac.kr>
2014-07-19 22:35:32 +0900

    Slack OAuth2.0 partially implemented.

--- notifyhere/dash/urls.py
+++ notifyhere/dash/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from dash import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^auth/(?P<service>[a-zA-Z]+)/$', views.auth, name='auth'),
 )
--- notifyhere/dash/views.py
+++ notifyhere/dash/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from urllib import quote
+
+BASE_REDIRECT_URL = "http://cert.kaist.in:8000/dash/auth/"
+SLACK_CLIENT_ID = "2160778816.2472356570"
+
+def slack_auth():
+    url  = "https://slack.com/oauth/authorize"
+    url += "?client_id=" + SLACK_CLIENT_ID
+    url += "&redirect_uri=" + quote(BASE_REDIRECT_URL + "slack")
+    return redirect(url)
 
 def index(request):
-    return render(request, 'dash/index.html', {})
+
+    if request.method == 'GET':
+        return render(request, 'dash/index.html', {})
+
+    if request.method == 'POST':
+        if request.POST['service'] == 'slack':
+            return slack_auth()
+
+def auth(request, service=None):
+    output  = 'Thx! you authorized my access to your ' + str(service) + ' account'
+
+    if 'code' in request.GET:
+        output += '<br>The code was ' + request.GET['code']
+
+    return HttpResponse(output)
