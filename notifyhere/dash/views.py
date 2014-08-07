788ee8a44187be88bed91554a2072c4bd971717a
blukat29 <yunjong@kaist.ac.kr>
2014-08-07 11:58:50 +0900

    Fix callback error checking in views.py

--- notifyhere/dash/views.py
+++ notifyhere/dash/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from api.tools import getAllApis, getApi
-import api.slack
-import api.github
 
 def index(request):
     services = getAllApis()
     args = []
     for service in services:
-        agent = getApi(service)
-        agent.unpack(request.session.get(service))
+        agent = getApi(service).unpack(request.session.get(service))
         args.append(agent)
     return render(request, 'dash/index.html', {'services':args})
 
@@ -21,27 +18,20 @@ def auth_redirect(request, service=None):
     return redirect(link)
 
 def auth_callback(request, service=None):
-    if 'code' in request.GET:
-        agent = getApi(service)
-        agent.unpack(request.session[service])
-        agent.oauth_callback(request.GET)
-        request.session[service] = agent.pack()
-        success = True
-    else:
-        success = False
-    return render(request, 'dash/callback.html', {'service':service, 'success':success})
+    agent = getApi(service).unpack(request.session[service])
+    agent.oauth_callback(request.GET)
+    request.session[service] = agent.pack()
+    return render(request, 'dash/callback.html', {'service':service, 'success':agent.is_auth})
 
 def auth_logout(request, service=None):
-    agent = getApi(service)
-    agent.unpack(request.session[service])
+    agent = getApi(service).unpack(request.session[service])
     agent.logout()
     request.session[service] = agent.pack()
     return redirect('/dash')
 
 def ajax(request, service=None):
     try:
-        agent = getApi(service)
-        agent.unpack(request.session[service])
+        agent = getApi(service).unpack(request.session[service])
         noti = agent.update()
         result = [(name, str(noti[name])) for name in sorted(noti, key=noti.get, reverse=True)]
         return render(request, 'dash/noti.html', {'result':result})
